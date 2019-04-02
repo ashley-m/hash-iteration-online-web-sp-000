9f54142491b6a98d253e50361621f058668dc9ac
Ashley Miller <asm2fe@virginia.edu>
2019-04-02 23:31:41 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |s, n|
+    if s == "suite_a" && n.start_with("A")
+      winner = n
+    end
+  end
+  winner
 end
